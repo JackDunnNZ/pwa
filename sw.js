267d13a07bf98e865140dff2ef6a8c09af083aa1
Jack Dunn <jack.dunn.nz@gmail.com>
2020-03-09 14:33:26 -0400

    Initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/index.html',
+       '/js/iai.js',
+       '/css/bb.css',
+       '/css/iai.css',
+       '/css/materialize.css',
+       '/css/materialize-icons.css',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
